9559c0077a467abd425002a6d0f236be63b05418
jmtechquindio <jmtechquindio@gmail.com>
2026-03-28 16:02:09 -0500

    Enable prerendering and set trailingSlash option
    Updated prerender setting to true and added trailingSlash option.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,31 +1,3 @@
-import { createBrowserClient, isBrowser, parseCookies } from '@supabase/ssr';
-import type { LayoutLoad } from './$types';
-import { env } from '$env/dynamic/public';
-
 export const ssr = false;
-export const prerender = false;
-
-export const load: LayoutLoad = async ({ fetch, data, depends }) => {
-  depends('supabase:auth');
-
-  const supabase = createBrowserClient(
-    env.PUBLIC_SUPABASE_URL!,
-    env.PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      global: {
-        fetch,
-      },
-      cookies: {
-        getAll() {
-          return data.cookies;
-        },
-      },
-    }
-  );
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  return { supabase, session, user: data.user };
-};
+export const prerender = true;
+export const trailingSlash = 'always';
